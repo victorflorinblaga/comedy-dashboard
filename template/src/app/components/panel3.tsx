a2f2a0b4e1b1d4d54f6b128a207e9d748c969e70
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-21 11:09:39 +0100

    updating contents of template/src/app/components/panel3.tsx

--- template/src/app/components/panel3.tsx
+++ template/src/app/components/panel3.tsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 
-function panel3() {
-    return (
-      <div>
-        <h1>Panel 3</h1>
-      </div>
-    );
-  }
-  
-  export default panel3;
-  
-  
+const Panel3 = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-80">
+        <CardContent>
+          <CardTitle className="text-center">
+            Why don't scientists trust atoms?
+          </CardTitle>
+          <p className="text-center">Because they make up everything!</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Panel3;
